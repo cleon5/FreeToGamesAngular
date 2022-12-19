971d700a48fb5aaa5841d36666deb817d7b95e86
cleon5 <cleon5@ucol.mx>
2022-12-19 11:14:41 -0600

    Home componet

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,23 +12,20 @@ export class HomeComponent implements OnInit {
   constructor(private readonly router: Router, private RestService: RestService){}
 
   title = 'FreeToGamesAngular';
-  /*
-  GetWindowsGames = "https://www.freetogame.com/api/filter?platform=pc"; 
-  GetExploracionGames = "https://www.freetogame.com/api/filter?tag=mmorpg";
-  GetFpsGames = "https://www.freetogame.com/api/filter?tag=fps";*/
-
-  WindowsGames = [540,521,517,516,5085,9,12,2,529,528,466];
+  WindowsGames = [540,521,517,516,508,9,12,2,529,528,466];
   ExploracionGames=[521,517,427,475,445,11,380,458,455,522];
   FpsGames = [540,516,508,538,380,515,5,9,12,2,466];
-
+  Games!:any;
+  GamesRecent!:Array<any>;
 
   ngOnInit(){
     this.cargarDatos();
   }
 
   public cargarDatos(){
-    this.RestService.get("https://www.freetogame.com/api/games").subscribe(resp => {
-      console.log(resp)
+    this.RestService.get("https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=release-date").subscribe(resp => {
+      this.Games=resp;
+      this.GamesRecent = this.Games.splice(0,3)
     })
 
   }
